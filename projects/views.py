64944eb1a8daf449596ba8d776f282df6ed894b2
Jose Ulises Quevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2025-01-24 22:58:05 -0600

    Starting serializers and views for projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import ProjectSerializer
+from .models import Project
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view(['GET'])
+def get_projects(request):
+    all_projects = Project.objects.all()
+    serializer = ProjectSerializer(all_projects, many=True)
+    return Response(serializer.data)
