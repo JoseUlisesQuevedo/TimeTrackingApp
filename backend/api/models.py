7b67d5542f8d5a190fb1de68e06863c320f809bf
Jose Ulises Quevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2025-01-30 11:11:57 -0600

    Added timeEntry model

--- backend/api/models.py
+++ backend/api/models.py
@@ -38,3 +38,20 @@ class Project(models.Model):
     def __str__(self):
         return self.project_name
 
+class TimeEntry(models.Model):
+
+    #How many minutes in that day
+    duration = models.FloatField()
+    #Who registers the time
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='time_entries')
+    #To which project
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='time_entries')
+    #For what day
+    entry_date = models.DateField()
+
+    #When is the entry created and updated
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.project.project_name} - {self.entry_date} - {self.duration} hours"
