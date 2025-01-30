486924848588adf2e5707ef66bd1a1a7414b3d4f
Jose Ulises Quevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2025-01-30 09:44:49 -0600

    Adding view for Project listing and creation

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,8 +3,20 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Project
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 # Create your views here.
+
+class ProjectListCreate(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return queryset
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
