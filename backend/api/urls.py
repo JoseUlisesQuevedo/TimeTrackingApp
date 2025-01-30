3fe10de089bf421a27b96c7e26e6125324d6bb73
Jose Ulises Quevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2025-01-30 12:37:14 -0600

    Added project update url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("projects/",views.ProjectListCreate.as_view(), name="project-list-create"),
     path("projects/delete/<int:pk>/",views.ProjectDelete.as_view(), name="project-delete"),
+    path("projects/update/<int:pk>/",views.ProjectUpdateView.as_view(), name="project-update"),
 ]
