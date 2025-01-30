594ea0e35d42db72e50d439c6c4ab6531e2b2650
Jose Ulises Quevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2025-01-30 12:48:23 -0600

    Added timeEntry endpoint to URLS

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("projects/",views.ProjectListCreate.as_view(), name="project-list-create"),
     path("projects/delete/<int:pk>/",views.ProjectDelete.as_view(), name="project-delete"),
     path("projects/update/<int:pk>/",views.ProjectUpdateView.as_view(), name="project-update"),
+    path("timeEntries/",views.TimeEntryListCreate.as_view(), name="time-entry-list-create"),
 ]
