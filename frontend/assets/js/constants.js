c6f2343715afec51c532a05f9c995a6a0d88ed74
Jose Ulises Quevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2025-01-30 23:35:59 -0600

    Added area and status translation

--- frontend/assets/js/constants.js
+++ frontend/assets/js/constants.js
@@ -1,2 +1,15 @@
 export const ACCESS_TOKEN = "accessToken";
 export const REFRESH_TOKEN = "refreshToken";
+
+export const AREA_MAPPING = {  
+    'C': 'Consulting',
+    'RI': 'Research Initiative',
+    'II': 'Internal Initiative',
+    'OTH': 'Other'
+};
+export const STATUS_MAPPING = { 
+    'A': 'Active',
+    'OH': 'On hold',
+    'C': 'Completed'
+};
+export const CACHE_EXPIRY = 60 * 60 * 1000; // 1 hour in milliseconds
