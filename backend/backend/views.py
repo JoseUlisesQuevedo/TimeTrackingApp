9f4324b42d23696ec2ea94704f44877b8b88a266
Jose Ulises Quevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2025-02-18 20:57:31 -0600

    Added login decorators to key views

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html')
 
+
+@login_required
 def projects(request):
     return render(request, 'project-details.html')
 
+@login_required
 def time_entry(request):
     return render(request, 'time-entries.html')
+
