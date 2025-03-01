12c269d0631e0f58cdc5f9be4a882a12fd7fdbd1
Jose Ulises Quevedo <47492091+JoseUlisesQuevedo@users.noreply.github.com>
2025-03-01 11:43:58 -0600

    Made access token expire on 1 day

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,7 +69,7 @@ REST_FRAMEWORK = {
 
 #Defines rules for authentication time expirations
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
